7c2fee0b2062bf56b5518121a7229e6b534a6e5a
Jose <jcisneros@repay.com>
2020-05-20 10:12:33 -0700

    Make async

--- src/Windows/02/Start_Here/StockAnalyzer.Windows/MainWindow.xaml.cs
+++ src/Windows/02/Start_Here/StockAnalyzer.Windows/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace StockAnalyzer.Windows
             InitializeComponent();
         }
 
-        private void Search_Click(object sender, RoutedEventArgs e)
+        private async void Search_Click(object sender, RoutedEventArgs e)
         {
             #region Before loading stock data
             var watch = new Stopwatch();
@@ -27,14 +27,24 @@ namespace StockAnalyzer.Windows
             StockProgress.IsIndeterminate = true;
             #endregion
 
-            var client = new WebClient();
-
-            var content = client.DownloadString($"http://localhost:61363/api/stocks/{Ticker.Text}");
-
-            var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content);
-
-            Stocks.ItemsSource = data;
-
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync($"http://localhost:61363/api/stocks/{Ticker.Text}");
+                try
+                {
+                    response.EnsureSuccessStatusCode();
+                    
+                    var content = await response.Content.ReadAsStringAsync();
+                    var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content);
+                
+                    Stocks.ItemsSource = data;
+                }
+                catch (Exception exception)
+                {
+                    Notes.Text += exception.Message;
+                }
+            }
+            
             #region After stock data is loaded
             StocksStatus.Text = $"Loaded stocks for {Ticker.Text} in {watch.ElapsedMilliseconds}ms";
             StockProgress.Visibility = Visibility.Hidden;
